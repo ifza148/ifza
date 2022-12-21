6dcb5b5c2a319f5cc7feaadf456d2686ff420377
hp <hp@UZAIR>
2022-12-21 11:33:14 +0500

    Add project files.

---
+++ task 3/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace task_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
